4375c0f3ce47047b808fd18016cabed177aa2119
AlexandreDresch <connectWithAlexandre@proton.me>
2025-08-08 23:58:25 -0300

    feat: enhance config for JWT secret and expiration

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,12 +6,18 @@ interface Config {
   port: number;
   nodeEnv: string;
   databaseUrl: string;
+  jwtSecret: string;
+  jwtExpiration: number;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: `.env.${process.env.NODE_ENV || 'development'}.local`,
   databaseUrl: process.env.DATABASE_URL || 'mongodb://localhost:27017/paynest',
+  jwtSecret: process.env.JWT_SECRET || 'defaultsecret',
+  jwtExpiration: process.env.JWT_EXPIRATION
+    ? Number(process.env.JWT_EXPIRATION)
+    : 86400,
 };
 
 export default config;
